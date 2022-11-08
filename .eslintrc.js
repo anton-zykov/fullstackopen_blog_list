d17985db1862133490a9c01108210b0b87cd50c7
Anton <anton.zykov1@gmail.com>
2022-11-08 23:38:14 +0300

    4.2 completed

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": ["airbnb", "airbnb/hooks"],
+    "parserOptions": {
+        "ecmaVersion": 14
+    },
+    "rules": {
+        'semi': [
+            'error',
+            'never'
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'no-console': 0,
+        'no-underscore-dangle': 'allow',
+    }
+};
